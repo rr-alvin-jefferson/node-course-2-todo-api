26cd80eaf6129a2cad152b92203c8b5ff1681573
Alvin Jefferson <ajefferson@ebates.com>
2018-06-10 20:25:04 -0500

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5b1dc72c3a9e7c4d882269a5')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    // Challenge update
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5b1db2dc45705d94f4fdbae6')
+    }, {
+        $set: {
+            name: 'Alvin'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
